5146b607ae6888ca7030602a5ec121bcc72d4ebf
Jael <jael.svd@gmail.com>
2019-03-09 20:32:32 -0800

    add technologies

--- src/components/help.jsx
+++ src/components/help.jsx
@@ -9,14 +9,316 @@ class Help extends Component {
           <div className="col-sm-12 text-center">
             <br />
             <br />
+            <h4>Technologies</h4>
+            <div class="card-deck">
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/html5.png")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">HTML 5</h5>
+                  <p class="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/css.png")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">CSS</h5>
+                  <p class="card-text">
+                    This card has supporting text below as a natural lead-in to
+                    additional content.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/javascript.jpg")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">JavaScript</h5>
+                  <p class="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This card has even longer
+                    content than the first to show that equal height action.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 text-center">
+            <br />
+            <div class="card-deck">
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/angularjs.png")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Angular</h5>
+                  <p class="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/react.png")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Reactjs</h5>
+                  <p class="card-text">
+                    This card has supporting text below as a natural lead-in to
+                    additional content.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/ionic.jpg")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">ionic</h5>
+                  <p class="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This card has even longer
+                    content than the first to show that equal height action.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 text-center">
+            <br />
+            <div class="card-deck">
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/ts.png")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">TypeScript</h5>
+                  <p class="card-text">
+                    TypeScript is very modern with the Angular Framework. I've
+                    been working a lot with this language.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/vs.png")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Visual Studio</h5>
+                  <p class="card-text">
+                    Work with various technologies within Visual Studio such as
+                    Asp.net, MVC framework, creation of APIs, Xamarin, Windows
+                    Azure.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/salesforce.png")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Sales Force</h5>
+                  <p class="card-text">
+                    By working with TouchCR we have to work with SalesForce,
+                    since TouchCR is a platform created within SalesForce.
+                    Worked with various segments such as agile, reports,
+                    dashboards, lightning components, interaction with APIs.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 text-center">
+            <br />
+            <div class="card-deck">
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/angularjs.png")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Angular</h5>
+                  <p class="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/react.png")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Reactjs</h5>
+                  <p class="card-text">
+                    This card has supporting text below as a natural lead-in to
+                    additional content.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/ionic.jpg")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">ionic</h5>
+                  <p class="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This card has even longer
+                    content than the first to show that equal height action.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 text-center">
+            <br />
+            <div class="card-deck">
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/sass.png")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Sass</h5>
+                  <p class="card-text">
+                    TypeScript is very modern with the Angular Framework. I've
+                    been working a lot with this language.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/GitHub.png")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">GitHub</h5>
+                  <p class="card-text">
+                    Work with various technologies within Visual Studio such as
+                    Asp.net, MVC framework, creation of APIs, Xamarin, Windows
+                    Azure.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+              <div class="card technology">
+                <img
+                  src={require("../img/versions/heroku.png")}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Heroku</h5>
+                  <p class="card-text">
+                    By working with TouchCR we have to work with SalesForce,
+                    since TouchCR is a platform created within SalesForce.
+                    Worked with various segments such as agile, reports,
+                    dashboards, lightning components, interaction with APIs.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 text-center">
             <br />
             <br />
-            <h3>Need Any Help?</h3>
+            <br />
+            <br />
             <h4>
               <i className="fa fa-thumbs-up fa-2x" /> I am available for
               freelance hire
             </h4>
-
             <nav className="cl-effect-2">
               <a href="#contact_me">
                 <span data-hover="Hire Me">Hire Me</span>
